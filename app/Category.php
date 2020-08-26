3a91e585df22b58658cf88576cdbb6bc8b0d1533
Waldemar3 <68418282+Waldemar3@users.noreply.github.com>
2020-08-26 12:12:22 +0300

    Update Category.php

--- app/Category.php
+++ app/Category.php
@@ -9,6 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     function post(){
-        return $this->hasMany(Post::class);
+        return $this->hasOne(Post::class);
     }
 }
